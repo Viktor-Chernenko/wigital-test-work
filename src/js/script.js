245add5b35e396e17ca9635a56cb5a730ea6f290
Viktor Chernenko <chernenkoviktor1991@gmail.com>
2020-02-05 15:43:28 +0300

    The test task is completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,47 @@
-// === slider ===
+if (document.readyState || document.body.readyState=='complete') {
 
-var mySwiper = new Swiper ('.swiper-container', {
+  // === slider ===
+
+  var mySwiper = new Swiper ('.swiper-container', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  })
+  });
+  
+  // === menu-mobile ===
+
+  let widthScreen = document.body.clientWidth;
+
+  if(widthScreen <= 850) {
+    let nav_mobile_btn = document.querySelector('.nav-btn'),
+        nav_mobile = document.querySelector('.nav-mobile__list');
+        
+    nav_mobile_btn.onclick = function () {
+      nav_mobile_btn.classList.toggle("active"),nav_mobile.classList.toggle("nav-mobile__list_active");
+    }
+  };
+
+  // === modal cal order ===
+
+  let modal = document.getElementById('call-order-modal'),
+      btn = document.getElementById('btn-call-order'),
+      span = document.getElementById('call-order-modal-close');
+  
+    btn.onclick = function() {
+      modal.style.display = "flex";
+    }
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
+
+
+
